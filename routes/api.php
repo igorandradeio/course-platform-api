7dbf28edd576e2dd8c94ac42b04a893cf777e161
igorandradeio <oigorwebs@gmail.com>
2022-04-06 16:55:06 -0300

    Create support feature

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\Api\{
     CourseController,
     LessonController,
-    ModuleController
+    ModuleController,
+    SupportController
 };
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,6 +16,8 @@ Route::get('/courses/{id}', [CourseController::class, 'show']);
 Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
 Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
 Route::get('/lessons/{id}', [LessonController::class, 'show']);
+Route::get('/users/{id}/supports', [SupportController::class, 'index']);
+
 
 Route::get('/', function () {
     return response()->json(['success' => true]);
