9c40c8fdc916c12501523e6ada811a137f03df2c
igorandradeio <oigorwebs@gmail.com>
2022-04-07 15:24:42 -0300

    Create view method

--- app/Http/Controllers/Api/LessonController.php
+++ app/Http/Controllers/Api/LessonController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreView;
 use App\Http\Resources\LessonResource;
 use App\Repositories\LessonRepository;
 use Illuminate\Http\Request;
@@ -28,5 +29,12 @@ class LessonController extends Controller
         $lesson = $this->repository->getLesson($id);
 
         return new LessonResource($lesson);
+    }
+
+    public function viewed(StoreView $request)
+    {
+        $this->repository->markLessonViewed($request->lesson);
+
+        return response()->json(['success' => true]);
     }
 }
--- app/Repositories/LessonRepository.php
+++ app/Repositories/LessonRepository.php
@@ -22,4 +22,21 @@ class LessonRepository
     {
         return $this->entity->findOrFail($entity);
     }
+
+
+    public function markLessonViewed(string $lessonId)
+    {
+        $user = auth()->user();
+
+        $view = $user->views()->where('lesson_id', $lessonId)->first();
+        if ($view) {
+            return $view->update([
+                'qty' => $view->qty + 1
+            ]);
+        }
+
+        return $user->views()->create([
+            'lesson_id' => $lessonId
+        ]);
+    }
 }
